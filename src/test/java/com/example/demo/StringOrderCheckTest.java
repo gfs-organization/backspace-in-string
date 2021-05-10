aeb351e766d5c837fb9c91c3a359512a2699643f
Brentt Smith <brentt.smith@gfs.com>
2021-05-10 17:27:58 -0400

    Sort Order Check code challenge added

---
+++ src/test/java/com/example/demo/StringOrderCheckTest.java
@@ -0,0 +1,61 @@
+package com.example.demo;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class StringOrderCheckTest {
+
+    private StringOrderCheck target;
+
+    @Before
+    public void setup() {
+        target = new StringOrderCheck();
+    }
+
+    @Test
+    public void shouldReturnOrderedStringWhenLettersNotInOrder() {
+        final String testString = "zbcdefghijklmnopqrstuawyxv";
+        String actual = runTimedTest(testString);
+
+        assertThat(actual, is("abcdefghijklmnopqrstuvwxyz"));
+    }
+
+    @Test
+    public void shouldReturnTrueWhenLettersAreInOrder() {
+        final String testString = "abcdefghijklmnop";
+        String actual = runTimedTest(testString);
+
+        assertThat(actual, is("in order"));
+    }
+
+    @Test
+    public void shouldReturnTrueWhenNumbersAreInOrder() {
+        final String testString = "1234";
+        String actual = runTimedTest(testString);
+
+        assertThat(actual, is("in order"));
+    }
+
+    private String runTimedTest(String testString) {
+        final int loops = 1000;
+        long[] times = new long[loops];
+
+        for (int x = 0; x < loops; x++) {
+            long start = System.nanoTime();
+            target.isStringInOrder(testString);
+            long end = System.nanoTime();
+            times[x] = end - start;
+        }
+
+        long averageTime = 0;
+        for (int x = 1; x < loops; x++) {
+            averageTime += times[x];
+        }
+
+        System.out.println("Average Nanos: " + averageTime / (loops - 1));
+        return target.isStringInOrder(testString);
+    }
+}
