5ff71a3133927051db646be9058b7793a60113b5
Brentt Smith <brentt.smith@gfs.com>
2021-05-26 17:39:09 -0400

    Rerun of Sonthosh second entry with new Benchmarking

--- src/main/java/com/gfs/backspaceinstring/BackspacesInString.java
+++ src/main/java/com/gfs/backspaceinstring/BackspacesInString.java
@@ -3,16 +3,11 @@ package com.gfs.backspaceinstring;
 public class BackspacesInString {
 
     public String solution(final String testString) {
-        final int length = testString.length();
-        final StringBuilder builder = new StringBuilder(length);
-        int backSpaceCount = 0;
+        final StringBuilder builder = new StringBuilder(testString.length());
         for (final char inputChar : testString.toCharArray()) {
             if (inputChar == '#') {
-                backSpaceCount++;
                 if (builder.length() > 0) {
                     builder.deleteCharAt(builder.length() - 1);
-                } else if (backSpaceCount > length / 2) {
-                    return "";
                 }
             } else {
                 builder.append(inputChar);
