adeaccaebbb2c370e3255bf0b8677cdd7f110289
Brentt Smith <brentt.smith@gfs.com>
2021-05-26 14:08:34 -0400

    Sonthosh Devaraj third entry

--- src/main/java/com/gfs/backspaceinstring/BackspacesInString.java
+++ src/main/java/com/gfs/backspaceinstring/BackspacesInString.java
@@ -2,12 +2,17 @@ package com.gfs.backspaceinstring;
 
 public class BackspacesInString {
 
-    public static String solution(final String testString) {
-        final StringBuilder builder = new StringBuilder(testString.length());
+    public String solution(final String testString) {
+        final int length = testString.length();
+        final StringBuilder builder = new StringBuilder(length);
+        int backSpaceCount = 0;
         for (final char inputChar : testString.toCharArray()) {
             if (inputChar == '#') {
+                backSpaceCount++;
                 if (builder.length() > 0) {
                     builder.deleteCharAt(builder.length() - 1);
+                } else if (backSpaceCount > length / 2) {
+                    return "";
                 }
             } else {
                 builder.append(inputChar);
