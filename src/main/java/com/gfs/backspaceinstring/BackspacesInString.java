87f5b632b2e834735144819e1383b10d9fe4208c
Brentt Smith <brentt.smith@gfs.com>
2021-05-26 08:41:15 -0400

    Sonthoshs second entry

--- src/main/java/com/gfs/backspaceinstring/BackspacesInString.java
+++ src/main/java/com/gfs/backspaceinstring/BackspacesInString.java
@@ -1,14 +1,10 @@
 package com.gfs.backspaceinstring;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.stream.Collectors;
-
 public class BackspacesInString {
 
     public static String solution(final String testString) {
-        StringBuilder builder = new StringBuilder(testString.length());
-        for (char inputChar : testString.toCharArray()) {
+        final StringBuilder builder = new StringBuilder(testString.length());
+        for (final char inputChar : testString.toCharArray()) {
             if (inputChar == '#') {
                 if (builder.length() > 0) {
                     builder.deleteCharAt(builder.length() - 1);
