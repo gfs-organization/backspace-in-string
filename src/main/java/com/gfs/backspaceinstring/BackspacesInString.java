869271b8b27ceb5b295a303c97d62d20d518baea
Brentt Smith <brentt.smith@gfs.com>
2021-05-26 22:13:56 -0400

    Kyle Niewiada third entry

--- src/main/java/com/gfs/backspaceinstring/BackspacesInString.java
+++ src/main/java/com/gfs/backspaceinstring/BackspacesInString.java
@@ -3,26 +3,22 @@ package com.gfs.backspaceinstring;
 public class BackspacesInString {
 
     public String solution(final String input) {
-        var tempResult = new char[input.length()];
+        var resultCharArray = new char[input.length()];
         var stringIndex = 0;
 
         for (var i = 0; i < input.length(); i++) {
-            stringIndex++; // Increase string index since we have a new char to look at
 
             // ASCII of # == 35
             if (input.charAt(i) == '#') {
-                stringIndex--; // Backspace itself.
                 if (stringIndex > 0) {
                     stringIndex--; // Delete previous char if index allows
                 }
             } else {
-                tempResult[stringIndex - 1] = input.charAt(i);
+                resultCharArray[stringIndex] = input.charAt(i);
+                stringIndex++; // Increase string index since we have a new char to look at
             }
         }
 
-        var resultBytes = new char[stringIndex];
-        System.arraycopy(tempResult, 0, resultBytes, 0, stringIndex);
-
-        return new String(resultBytes);
+        return new String(resultCharArray, 0, stringIndex);
     }
 }
