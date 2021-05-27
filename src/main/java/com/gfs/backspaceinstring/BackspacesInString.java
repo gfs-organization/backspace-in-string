69fc368b09d6a241691c9c24cfaae7efc2202b73
Brentt Smith <brentt.smith@gfs.com>
2021-05-27 10:17:15 -0400

    Harish Theranikal third entry - failing tests

--- src/main/java/com/gfs/backspaceinstring/BackspacesInString.java
+++ src/main/java/com/gfs/backspaceinstring/BackspacesInString.java
@@ -5,14 +5,20 @@ public class BackspacesInString {
 
     public static String solution(final String inputString) {
         final StringBuilder sb = new StringBuilder();
+        int backSpaceCount = 0;
         for (final char c : inputString.toCharArray()) {
             if (c == BACK_SPACE) {
-                if (sb.length() > 0) {
-                    sb.setLength(sb.length() - 1);
-                }
+                ++backSpaceCount;
             } else {
+                if (backSpaceCount > sb.length()) {
+                    sb.setLength(0);
+                } else {
+                    sb.setLength(sb.length() - backSpaceCount);
+                }
+                backSpaceCount = 0;
                 sb.append(c);
             }
+
         }
         return sb.toString();
     }
