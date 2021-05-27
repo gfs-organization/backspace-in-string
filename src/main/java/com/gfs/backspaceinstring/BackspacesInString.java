c70b079e0baa14e9ed016dd4a439c6c39575d916
Brentt Smith <brentt.smith@gfs.com>
2021-05-26 22:00:50 -0400

    Kyle Niewiada second entry

--- src/main/java/com/gfs/backspaceinstring/BackspacesInString.java
+++ src/main/java/com/gfs/backspaceinstring/BackspacesInString.java
@@ -1,31 +1,28 @@
 package com.gfs.backspaceinstring;
 
-import java.nio.charset.StandardCharsets;
-
 public class BackspacesInString {
 
     public String solution(final String input) {
-        var inputBytes = input.getBytes(StandardCharsets.US_ASCII);
-        var tempResult = new byte[input.length()];
+        var tempResult = new char[input.length()];
         var stringIndex = 0;
 
         for (var i = 0; i < input.length(); i++) {
             stringIndex++; // Increase string index since we have a new char to look at
 
             // ASCII of # == 35
-            if (inputBytes[i] == 35) {
+            if (input.charAt(i) == '#') {
                 stringIndex--; // Backspace itself.
                 if (stringIndex > 0) {
                     stringIndex--; // Delete previous char if index allows
                 }
             } else {
-                tempResult[stringIndex - 1] = inputBytes[i];
+                tempResult[stringIndex - 1] = input.charAt(i);
             }
         }
 
-        var resultBytes = new byte[stringIndex];
+        var resultBytes = new char[stringIndex];
         System.arraycopy(tempResult, 0, resultBytes, 0, stringIndex);
 
-        return new String(resultBytes, StandardCharsets.US_ASCII);
+        return new String(resultBytes);
     }
 }
