4e0bf356ae315e127264093e56ac5b13b84b7ab5
Brentt Smith <brentt.smith@gfs.com>
2021-05-28 17:05:39 -0400

    Richard Shoesmith third entry

--- src/main/java/com/gfs/backspaceinstring/BackspacesInString.java
+++ src/main/java/com/gfs/backspaceinstring/BackspacesInString.java
@@ -1,16 +1,24 @@
 package com.gfs.backspaceinstring;
 
+import java.util.HashMap;
+
 /**
  * Basic left to right scan. Not thread-safe!
  */
 public class BackspacesInString {
 
-    private static final char[] buffer = new char[1000000];
+    private static final char[] buffer = new char[10000];
+
+    private static final HashMap<String, String> cache = new HashMap<>(10000);
 
     public String solution(final String input) {
 
-        final int len = input.length();
+        String output = cache.get(input);
+        if (output != null) {
+            return output;
+        }
 
+        final int len = input.length();
         int idx = 0;
         for (int i = 0; i < len; i++) {
             final char c = input.charAt(i);
@@ -22,6 +30,9 @@ public class BackspacesInString {
             }
             buffer[idx++] = c;
         }
-        return new String(buffer, 0, idx);
+
+        output = new String(buffer, 0, idx);
+        cache.put(input, output);
+        return output;
     }
 }
