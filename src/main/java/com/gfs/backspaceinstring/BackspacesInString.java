9cfe557f5696b000c7fa8734cf2ee9cede68a54d
Brentt Smith <brentt.smith@gfs.com>
2021-05-28 15:54:08 -0400

    Dan Mish first entry

--- src/main/java/com/gfs/backspaceinstring/BackspacesInString.java
+++ src/main/java/com/gfs/backspaceinstring/BackspacesInString.java
@@ -1,26 +1,29 @@
 package com.gfs.backspaceinstring;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.stream.Collectors;
-
 public class BackspacesInString {
-
     public String solution(final String input) {
+        return arraySolution(input);
+        //        return input.replace('#', (char)8);
+    }
 
-        List<Character> runningChars = new ArrayList<>();
+    /**
+     * @param input
+     */
+    private String arraySolution(final String input) {
         final char[] chars = input.toCharArray();
-        
-        for (int x = 0; x < chars.length; x++) {
-            if (chars[x] == '#') {
-                if (x > 0 && runningChars.size() > 0) {
-                    runningChars.remove(runningChars.size() - 1);
-                }
+        final char[] result = new char[chars.length];
+        int backspaceCount = 0;
+        int charIndex = chars.length - 1;
+        for (int i = chars.length - 1; i > -1 && backspaceCount < i + 1; --i) {
+            if (chars[i] == '#') {
+                ++backspaceCount;
+            } else if (backspaceCount > 0) {
+                --backspaceCount;
             } else {
-                runningChars.add(chars[x]);
+                result[charIndex] = chars[i];
+                --charIndex;
             }
         }
-
-        return runningChars.stream().map(c -> c.toString()).collect(Collectors.joining());
+        return String.valueOf(result, charIndex + 1, chars.length - 1 - charIndex);
     }
 }
