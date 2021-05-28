9a909e8660b48a6094d97179c87948e868549396
Brentt Smith <brentt.smith@gfs.com>
2021-05-28 15:30:30 -0400

    Richard Shoesmith second entry

--- src/main/java/com/gfs/backspaceinstring/BackspacesInString.java
+++ src/main/java/com/gfs/backspaceinstring/BackspacesInString.java
@@ -1,27 +1,27 @@
 package com.gfs.backspaceinstring;
 
 /**
- * Basic left to right scan
+ * Basic left to right scan. Not thread-safe!
  */
 public class BackspacesInString {
 
+    private static final char[] buffer = new char[1000000];
+
     public String solution(final String input) {
 
-        final char[] arr = input.toCharArray();
-        final int len = arr.length;
+        final int len = input.length();
 
         int idx = 0;
         for (int i = 0; i < len; i++) {
-            final char c = arr[i];
+            final char c = input.charAt(i);
             if (c == '#') {
                 if (idx > 0) {
                     idx--;
                 }
                 continue;
             }
-            arr[idx++] = c;
+            buffer[idx++] = c;
         }
-        return new String(arr, 0, idx);
+        return new String(buffer, 0, idx);
     }
-
 }
