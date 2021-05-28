03149be873e4b36b71194ca005f57047273e912f
Brentt Smith <brentt.smith@gfs.com>
2021-05-28 13:34:39 -0400

    Seth Janes second entry

--- src/main/java/com/gfs/backspaceinstring/BackspacesInString.java
+++ src/main/java/com/gfs/backspaceinstring/BackspacesInString.java
@@ -1,9 +1,8 @@
 package com.gfs.backspaceinstring;
 
 public class BackspacesInString {
-
     public String solution(final String input) {
-        Character[] temp = new Character[input.length()];
+        char[] output = new char[input.length()];
 
         int currentIndex = 0;
         char[] inputArray = input.toCharArray();
@@ -13,17 +12,11 @@ public class BackspacesInString {
                 //Don't need to actually delete the character, just move back and overwrite it later if necessary
                 currentIndex = Math.max(currentIndex - 1, 0);
             } else {
-                temp[currentIndex] = currentCharacter;
+                output[currentIndex] = currentCharacter;
                 currentIndex++;
             }
         }
 
-        StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < currentIndex; i++) {
-            //The array could be huge, but if our index is at 0 it won't print.
-            sb.append(temp[i]);
-        }
-
-        return sb.toString();
+        return String.valueOf(output, 0, currentIndex);
     }
 }
