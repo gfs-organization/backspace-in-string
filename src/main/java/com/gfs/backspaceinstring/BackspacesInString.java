cfdee73a7f73f044f44d3d47f35b2a549f08f35a
Brentt Smith <brentt.smith@gfs.com>
2021-05-28 17:04:23 -0400

    Greg King fifth entry - failed tests

--- src/main/java/com/gfs/backspaceinstring/BackspacesInString.java
+++ src/main/java/com/gfs/backspaceinstring/BackspacesInString.java
@@ -1,22 +1,30 @@
 package com.gfs.backspaceinstring;
 
+import java.lang.reflect.Field;
+
 public class BackspacesInString {
     public String solution(final String input) {
-        final char[] chars = input.toCharArray();
-        int backspaceCount = 0;
-        int currChar = input.length();
-        for (int i = currChar - 1; i >= 0; i--) {
-            if (chars[i] == '#') {
-                backspaceCount++;
-            } else if (backspaceCount > 0) {
-                backspaceCount--;
-            } else {
-                currChar--;
-                if (currChar != i) {
-                    chars[currChar] = chars[i];
+        try {
+            final Field field = String.class.getDeclaredField("value");
+            field.setAccessible(true);
+            final byte[] chars = (byte[]) field.get(input);
+            int backspaceCount = 0;
+            int currChar = input.length();
+            for (int i = currChar - 1; i >= 0; i--) {
+                if (chars[i] == '#') {
+                    backspaceCount++;
+                } else if (backspaceCount > 0) {
+                    backspaceCount--;
+                } else {
+                    currChar--;
+                    if (currChar != i) {
+                        chars[currChar] = chars[i];
+                    }
                 }
             }
+            return new String(chars, currChar, input.length() - currChar);
+        } catch (final Exception thisIsAGoodIdea) {
+            return "";
         }
-        return new String(chars, currChar, input.length() - currChar);
     }
 }
