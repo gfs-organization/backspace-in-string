cdd28b8c34eca9be36c7588d1813c741b211953b
Brentt Smith <brentt.smith@gfs.com>
2021-05-30 06:44:13 -0400

    Tom Jahncke second entry

--- src/main/java/com/gfs/backspaceinstring/BackspacesInString.java
+++ src/main/java/com/gfs/backspaceinstring/BackspacesInString.java
@@ -1,29 +1,20 @@
 package com.gfs.backspaceinstring;
 
-import java.util.HashSet;
-import java.util.Set;
-
 public class BackspacesInString {
 
     public String solution(final String input) {
-        StringBuilder sb = new StringBuilder(input.length());
-
-        int index = input.indexOf('#');
-        Set<Integer> backspaceIndexes = new HashSet<>(input.length());
-        while (index >= 0) {
-            backspaceIndexes.add(index);
-            index = input.indexOf('#', index + 1);
-        }
+        char[] result = new char[input.length()];
         char[] chars = input.toCharArray();
+        int y = 0;
         for (int i = 0; i < chars.length; i++) {
-            if (sb.length() > 0 && backspaceIndexes.contains(i)) {
-                sb.deleteCharAt(sb.length() - 1);
-            } else if (chars[i] == '#') {
-                continue;
-            } else {
-                sb.append(chars[i]);
+            char aChar = chars[i];
+            if (aChar == '#' && y > 0) {
+                y--;
+            } else if (aChar != '#') {
+                result[y] = aChar;
+                y++;
             }
         }
-        return sb.toString();
+        return String.valueOf(result, 0, y);
     }
 }
