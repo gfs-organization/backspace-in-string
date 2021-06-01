33245f75d50d08910cf42536fb815e8e27506bc5
Brentt Smith <brentt.smith@gfs.com>
2021-06-01 16:04:35 -0400

    Travis Mcgeehan sixth entry

--- src/main/java/com/gfs/backspaceinstring/BackspacesInString.java
+++ src/main/java/com/gfs/backspaceinstring/BackspacesInString.java
@@ -3,12 +3,12 @@ package com.gfs.backspaceinstring;
 public class BackspacesInString {
     public String solution(final String testString) {
         final int testStringLength = testString.length();
-        final char[] chars = new char[testStringLength];
+        final byte[] chars = new byte[testStringLength];
         int index = 0;
         int outputLength = 0;
         while (index < testStringLength) {
-            final char thisChar = testString.charAt(index++);
-            if (thisChar == '#') {
+            final byte thisChar = (byte) testString.charAt(index++);
+            if (thisChar == 35) {
                 outputLength = Math.max(outputLength - 1, 0);
             } else {
                 chars[outputLength++] = thisChar;
